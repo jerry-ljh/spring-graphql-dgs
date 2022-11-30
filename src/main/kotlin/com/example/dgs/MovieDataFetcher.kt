da44d226474521f6d1d8f1ef3b983160c44812bd
jerry.hj <jerry.hj@kakaostyle.com>
2022-11-30 21:26:09 +0900

    feat: movie query

---
+++ src/main/kotlin/com/example/dgs/MovieDataFetcher.kt
@@ -0,0 +1,43 @@
+package com.example.dgs
+
+import com.example.dgs.generated.DgsConstants
+import com.example.dgs.generated.types.Actor
+import com.example.dgs.generated.types.Movie
+import com.example.dgs.generated.types.Music
+import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsData
+import com.netflix.graphql.dgs.DgsDataFetchingEnvironment
+import com.netflix.graphql.dgs.DgsQuery
+import org.dataloader.DataLoader
+import java.util.concurrent.CompletableFuture
+
+
+@DgsComponent
+class MovieDataFetcher {
+    private val movies = listOf(
+        Movie(title = { "Stranger Things" }),
+        Movie(title = { "Ozark" }),
+        Movie(title = { "The Crown" }),
+        Movie(title = { "Dead to Me" }),
+        Movie(title = { "Orange is the New Black" })
+    )
+
+    @DgsQuery(field = DgsConstants.QUERY.Movies)
+    fun movies(): List<Movie> {
+        return movies
+    }
+
+    @DgsData(parentType = DgsConstants.MOVIE.TYPE_NAME)
+    fun actors(dfe: DgsDataFetchingEnvironment): CompletableFuture<List<Actor>> {
+        val dataloader: DataLoader<String, List<Actor>> = dfe.getDataLoader(ActorDataloader::class.java)
+        val movie: Movie = dfe.getSource()
+        return dataloader.load(movie.title)
+    }
+
+    @DgsData(parentType = DgsConstants.MOVIE.TYPE_NAME)
+    fun music(dfe: DgsDataFetchingEnvironment): CompletableFuture<Music> {
+        val dataloader: DataLoader<String, Music> = dfe.getDataLoader(MusicDataloader::class.java)
+        val movie: Movie = dfe.getSource()
+        return dataloader.load(movie.title)
+    }
+}
